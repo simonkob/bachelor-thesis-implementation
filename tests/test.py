2b148a40e65e07b0b1e87d45bd39d6dfb9041dbe
Šimon Bezek <xbezek2@fi.muni.cz>
2021-05-11 04:11:13 +0200

    introducing test for get_trusted_users

---
+++ tests/test.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest import mock
+from main import get_trusted_users, InfoOptions
+
+
+def mock_basic(*args):
+    if args[0] == InfoOptions.following:
+        return {
+            "AlienVault": ["user1"],
+            "user1": ["user2"]
+        }.get(args[1], [])
+    else:
+        return {
+            "AlienVault": ["user3"],
+            "user3": ["user4"]
+        }.get(args[1], [])
+
+
+class GetTrustedUsersTestCase(unittest.TestCase):
+    @mock.patch("main.get_watched_users", side_effect=mock_basic)
+    def test_basic(self, mock_get):
+        result = get_trusted_users("AlienVault", 7)
+        self.assertEqual(result[0], {'user1', 'user2'})
+        self.assertEqual(result[1], {'AlienVault', 'user3', 'user4'})
+
+
+if __name__ == "__main__":
+    unittest.main()
