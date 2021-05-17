e9115f7d5786933f1d7ade88f881092ce7686937
Šimon Bezek <xbezek2@fi.muni.cz>
2021-05-17 15:56:38 +0200

    Adding test_case1 and test_repeat to GetTrustedUsersTestCase

--- tests/test.py
+++ tests/test.py
@@ -25,6 +25,33 @@ def mock_basic(*args):
         }.get(args[1], [])
 
 
+def mock_case1(*args):
+    if args[0] == InfoOptions.following:
+        return {
+            "AlienVault": ["user1"],
+            "user1": ["user2"],
+            "user3": ["user4"]
+        }.get(args[1], [])
+    else:
+        return {
+            "user2": ["user3"]
+        }.get(args[1], [])
+
+
+def mock_repeat(*args):
+    if args[0] == InfoOptions.following:
+        return {
+            "AlienVault": ["u2", "u3", "u4"],
+            "u4": ["AlienVault", "u1", "u2"]
+        }.get(args[1], [])
+    else:
+        return {
+            "AlienVault": ["u1"],
+            "u1": ["u3", "u4"],
+            "u3": ["u4"]
+        }.get(args[1], [])
+
+
 class GetTrustedUsersTestCase(unittest.TestCase):
     @mock.patch("main.get_watched_users", side_effect=mock_empty)
     def test_empty(self, mock_get):
@@ -37,6 +64,18 @@ class GetTrustedUsersTestCase(unittest.TestCase):
         result = get_trusted_users("AlienVault", 7)
         self.assertEqual(result[0], {'user1', 'user2'})
         self.assertEqual(result[1], {'AlienVault', 'user3', 'user4'})
+
+    @mock.patch("main.get_watched_users", side_effect=mock_case1)
+    def test_case1(self, mock_get):
+        result = get_trusted_users("AlienVault", 7)
+        self.assertEqual(result[0], {'user1', 'user2', 'user3', 'user4'})
+        self.assertEqual(result[1], {'AlienVault'})
+
+    @mock.patch("main.get_watched_users", side_effect=mock_repeat)
+    def test_repeat(self, mock_get):
+        result = get_trusted_users("AlienVault", 7)
+        self.assertEqual(result[0], {'u2'})
+        self.assertEqual(result[1], {'AlienVault', 'u1', 'u3', 'u4'})
 
 
 if __name__ == "__main__":
