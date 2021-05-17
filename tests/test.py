24947aedb480b1628658c475b27de4adf7ade069
Šimon Bezek <xbezek2@fi.muni.cz>
2021-05-17 15:36:04 +0200

    Adding test_empty to GetTrustedUsersTestCase

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,15 @@ from unittest import mock
 from main import get_trusted_users, InfoOptions
 
 
+def mock_empty(*args):
+    if args[0] == InfoOptions.following:
+        return {
+        }.get(args[1], [])
+    else:
+        return {
+        }.get(args[1], [])
+
+
 def mock_basic(*args):
     if args[0] == InfoOptions.following:
         return {
@@ -17,6 +26,12 @@ def mock_basic(*args):
 
 
 class GetTrustedUsersTestCase(unittest.TestCase):
+    @mock.patch("main.get_watched_users", side_effect=mock_empty)
+    def test_empty(self, mock_get):
+        result = get_trusted_users("AlienVault", 7)
+        self.assertEqual(result[0], set())
+        self.assertEqual(result[1], {'AlienVault'})
+
     @mock.patch("main.get_watched_users", side_effect=mock_basic)
     def test_basic(self, mock_get):
         result = get_trusted_users("AlienVault", 7)
