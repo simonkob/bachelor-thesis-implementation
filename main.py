521e8e6efecb0715d00879681398aa4c65dea377
Šimon Bezek <xbezek2@fi.muni.cz>
2022-05-10 19:41:35 +0200

    MITRE ATT&CK import now imports the latest version

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from OTXv2 import OTXv2
-from app import App
-import datetime
 import configparser
+import datetime
 import json
 import os
+import urllib.request
+
+from OTXv2 import OTXv2
+
+from app import App
 
 otx = OTXv2(api_key)  # Replace with your own OTX api key
 is_in_docker = os.getenv('IS_IN_DOCKER', False)
@@ -16,12 +19,29 @@ def create_pulses(app_, since=None):
     save_timestamp(config)
 
 
-def import_attack_json(app_):
-    with open("enterprise-attack-10.1.json") as file:
-        data = json.load(file)
-    json_objects_dict = {}
-    for item in data["objects"]:
-        app_.create_attack_item(item, json_objects_dict)
+def import_attack_json(app_, config_):
+    url = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack" \
+          ".json "
+    data = json.loads(urllib.request.urlopen(url).read())
+    data_version = data["objects"][0]["x_mitre_version"]
+    if data_version != load_attack_ver(config_):
+        json_objects_dict = {}
+        for item in data["objects"]:
+            app_.create_attack_item(item, json_objects_dict)
+        save_attack_ver(config_, data_version)
+    else:
+        print("Latest MITRE ATT&CK already imported.")
+
+
+def load_attack_ver(config_):
+    config_.read('config.ini')
+    return config_.get('Info', 'Attack_ver', fallback=None)
+
+
+def save_attack_ver(config_, version):
+    config_['Info'] = {'Attack_ver': version}
+    with open('config.ini', 'w') as config_file:
+        config_.write(config_file)
 
 
 def load_timestamp(config_):
