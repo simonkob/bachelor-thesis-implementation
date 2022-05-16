238b7f1c7d102c430bca9cacdded6a5150149f9b
Šimon Bezek <xbezek2@fi.muni.cz>
2022-05-16 09:40:45 +0200

    Adding docstrings to main.py

--- main.py
+++ main.py
@@ -14,12 +14,22 @@ config = configparser.ConfigParser()
 
 
 def create_pulses(app_, since=None):
+    """Creates a pulse record in the database for every pulse that has been modified since 'since' argument.
+
+    :param app_: A database connection
+    :param since: Date of last modification
+    """
     for pulse in otx.getall_iter(modified_since=since):
         app_.create_pulse(pulse)
     save_timestamp(config)
 
 
 def import_attack_json(app_, config_):
+    """Imports data from MITRE ATT&CK json
+
+    :param app_: A database connection
+    :param config_: Configuration file
+    """
     url = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack" \
           ".json "
     data = json.loads(urllib.request.urlopen(url).read())
@@ -34,22 +44,41 @@ def import_attack_json(app_, config_):
 
 
 def load_attack_ver(config_):
+    """Loads the last imported attack version from the configuration file
+
+    :param config_: Configuration file
+    :return: String of the last attack version
+    """
     config_.read('config.ini')
     return config_.get('Info', 'Attack_ver', fallback=None)
 
 
 def save_attack_ver(config_, version):
+    """Saves the last imported attack version to the configuration file
+
+    :param config_: Configuration file
+    :param version: Attack version to be saved
+    """
     config_.set('Info', 'Attack_ver', version)
     with open('config.ini', 'w') as config_file:
         config_.write(config_file)
 
 
 def load_timestamp(config_):
+    """Loads the date when pulses from OTX were last updated from configuration file
+
+    :param config_: Configuration file
+    :return: String of the date when pulses were last updated
+    """
     config_.read('config.ini')
     return config_.get('Info', 'Date', fallback=None)
 
 
 def save_timestamp(config_):
+    """Saves the date when pulses from OTX were last updated to configuration file
+
+    :param config_: Configuration file
+    """
     config_.set('Info', 'Date', str(datetime.datetime.now()))
     with open('config.ini', 'w') as config_file:
         config_.write(config_file)
